cbbc8953727be6b0dcf709dc346736d2701c258c
Cathybiwott <biwottcathy@gmail.com>
2023-07-04 14:22:50 +0300

    first commit

---
+++ src/Content/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './style.css';
+
+const Content = ()=>{
+    return(
+        <div>
+            <h2>I am Interested to learn React</h2>
+            <p>It is an interesting framework of JavaScript for a developer to learn</p>
+            <p>It has components that are reusable that are independent and reusable bits of code.</p>
+            <p>React is a library for JavaScript</p>
+            <p>Examples of frameworks of JavaScript include:</p>
+            <p>Angular,Node.js,Vue.js among others</p>
+        </div>
+    )
+}
+
+export default Content;
